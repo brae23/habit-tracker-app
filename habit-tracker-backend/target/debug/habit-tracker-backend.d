920bfa0febf2858709337fcdad04fedfdd4564c8
Brendan Rae <brae23hockey@gmail.com>
2023-06-05 20:35:32 -0400

    change folder layout and stand up backend project

---
+++ habit-tracker-backend/target/debug/habit-tracker-backend.d
@@ -0,0 +1,1 @@
+C:\Users\Brendan\Documents\Repos\habit-tracker-app\habit-tracker-backend\target\debug\habit-tracker-backend.exe: C:\Users\Brendan\Documents\Repos\habit-tracker-app\habit-tracker-backend\src\main.rs
